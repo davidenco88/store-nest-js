9d05d76ad313b51fc1b4398a98ed1a52b45f84cb
David Sarria <david.sarriav@gmail.com>
2024-06-04 11:58:41 +0200

    feat: :construction: Create the project's folder structure and initialize controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './features/users/users.controller';
+import { CategoriesController } from './features/categories/categories.controller';
+import { ProductsController } from './features/products/products.controller';
+import { OrdersController } from './features/orders/orders.controller';
+import { BrandsController } from './features/brands/brands.controller';
+import { CustomersController } from './features/customers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    UsersController,
+    CategoriesController,
+    ProductsController,
+    OrdersController,
+    BrandsController,
+    CustomersController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
