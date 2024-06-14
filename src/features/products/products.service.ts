6f38d662e641a640efd0c118e30782e9bd65623c
David Sarria <david.sarriav@gmail.com>
2024-06-14 16:43:32 +0200

    perf: :children_crossing: Improve Handle error Products services

--- src/features/products/products.service.ts
+++ src/features/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product, ProductUpdated } from './products.dto';
 import { randomUUID } from 'crypto';
 
@@ -20,24 +20,42 @@ export class ProductsService {
   }
 
   getById(id: string): Product {
-    return this.products.find((product) => product.id === id);
+    const product = this.products.find((item) => item.id === id);
+
+    if (!product) {
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+
+    return product;
   }
 
+  //Pendiente la validación del Payload
   create(product: Product): Product {
     const newId: string = randomUUID().toString();
     this.products.push({ ...product, id: newId });
     return this.getById(newId);
   }
 
+  //Pendiente la validación del Payload
   update(id: string, payload: ProductUpdated): Product {
     const index = this.products.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+
     this.products[index] = { ...this.products[index], ...payload };
     return this.getById(id);
   }
 
-  deleteById(id: string): { message: string } {
+  deleteById(id: string): { isDelted: boolean; message: string } {
     const index = this.products.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+
     this.products.splice(index, 1);
-    return { message: `Product ${id} was deleted succesfully` };
+    return { isDelted: true, message: `Product ${id} was deleted succesfully` };
   }
 }
