4fc53601aaa8faff04888dadf51a0c0d15a32e02
David Sarria <david.sarriav@gmail.com>
2024-09-16 23:04:47 +0200

    refactor: :recycle: Separated individuals modules for each feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './features/users/users.controller';
-import { CategoriesController } from './features/categories/categories.controller';
-import { ProductsController } from './features/products/products.controller';
-import { OrdersController } from './features/orders/orders.controller';
-import { BrandsController } from './features/brands/brands.controller';
-import { CustomersController } from './features/customers/customers.controller';
-import { ProductsService } from './features/products/products.service';
-import { BrandsService } from './features/brands/brands.service';
-import { CategoriesService } from './features/categories/categories.service';
-import { CustomersService } from './feature/customers/customers.service';
-import { UsersService } from './features/users/users.service';
+import { BrandsModule } from './brands/brands.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UsersController,
-    CategoriesController,
-    ProductsController,
-    OrdersController,
-    BrandsController,
-    CustomersController,
+  imports: [
+    BrandsModule,
+    CategoriesModule,
+    CustomersModule,
+    OrdersModule,
+    ProductsModule,
+    UsersModule,
   ],
-  providers: [
-    AppService,
-    ProductsService,
-    BrandsService,
-    CategoriesService,
-    CustomersService,
-    UsersService,
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
